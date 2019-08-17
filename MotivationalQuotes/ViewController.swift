0d76fab00f1f35813a3e11a9d62d329b4668604b
Siddharth <siddharth952@gmail.com>
2019-08-17 21:11:36 +0530

    Initial Commit

---
+++ MotivationalQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotivationalQuotes
+//
+//  Created by Siddharth Sen on 17/08/19.
+//  Copyright © 2019 halfpolygon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
